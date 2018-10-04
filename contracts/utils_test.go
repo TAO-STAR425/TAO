BuildOnViction/tomochain-v1

Get randomize m2 offer from validators in smart contract.
Fixed get and build randomize for m2 from smart contract.

Fixed get m2 list from smart contract randomize.

contracts/utils_test.go
@@ -16,12 +16,14 @@
 package contracts
 
 import (
+	"bytes"
 	"context"
 	"crypto/ecdsa"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind/backends"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/contracts/blocksigner"
+	"github.com/ethereum/go-ethereum/contracts/randomize"
 	"github.com/ethereum/go-ethereum/core"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
@@ -31,22 +33,30 @@ import (
 	"time"
 )
 
-func TestSendTxSign(t *testing.T) {
-	acc1Key, _ := crypto.HexToECDSA("8a1f9a8f95be41cd7ccb6168179afb4504aefe388d1e14474d32c45c72ce7b7a")
-	acc2Key, _ := crypto.HexToECDSA("49a7b37aa6f6645917e7b807e9d1c00d4fa71f18343b0d4122a4d2df64dd6fee")
-	acc3Key, _ := crypto.HexToECDSA("b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291")
-	acc4Key, _ := crypto.HexToECDSA("b71c71a67e1177ad4e901695e1b4b9ee04aefe388d1e14474d32c45c72ce7b7a")
-	acc1Addr := crypto.PubkeyToAddress(acc1Key.PublicKey)
-	acc2Addr := crypto.PubkeyToAddress(acc2Key.PublicKey)
-	acc3Addr := crypto.PubkeyToAddress(acc3Key.PublicKey)
-	acc4Addr := crypto.PubkeyToAddress(acc4Key.PublicKey)
-	accounts := []common.Address{acc2Addr, acc3Addr, acc4Addr}
-	keys := []*ecdsa.PrivateKey{acc2Key, acc3Key, acc4Key}
+var (
+	acc1Key, _ = crypto.HexToECDSA("8a1f9a8f95be41cd7ccb6168179afb4504aefe388d1e14474d32c45c72ce7b7a")
+	acc2Key, _ = crypto.HexToECDSA("49a7b37aa6f6645917e7b807e9d1c00d4fa71f18343b0d4122a4d2df64dd6fee")
+	acc3Key, _ = crypto.HexToECDSA("b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291")
+	acc4Key, _ = crypto.HexToECDSA("b71c71a67e1177ad4e901695e1b4b9ee04aefe388d1e14474d32c45c72ce7b7a")
+	acc1Addr   = crypto.PubkeyToAddress(acc1Key.PublicKey)
+	acc2Addr   = crypto.PubkeyToAddress(acc2Key.PublicKey)
+	acc3Addr   = crypto.PubkeyToAddress(acc3Key.PublicKey)
+	acc4Addr   = crypto.PubkeyToAddress(acc4Key.PublicKey)
+)
 
-	signer := types.HomesteadSigner{}
-	genesis := core.GenesisAlloc{acc1Addr: {Balance: big.NewInt(1000000000)}}
+func getCommonBackend() *backends.SimulatedBackend {
+	genesis := core.GenesisAlloc{acc1Addr: {Balance: big.NewInt(1000000000000)}}
 	backend := backends.NewSimulatedBackend(genesis)
 	backend.Commit()
+
+	return backend
+}
+
+func TestSendTxSign(t *testing.T) {
+	accounts := []common.Address{acc2Addr, acc3Addr, acc4Addr}
+	keys := []*ecdsa.PrivateKey{acc2Key, acc3Key, acc4Key}
+	backend := getCommonBackend()
+	signer := types.HomesteadSigner{}
 	ctx := context.Background()
 
 	transactOpts := bind.NewKeyedTransactor(acc1Key)
@@ -114,4 +124,176 @@ func randomHash() common.Hash {
 		b[i] = letterBytes[rand.Intn(len(letterBytes))]
 	}
 	return b
+}
+
+// Unit test for get random position of masternodes.
+func TestRandomMasterNode(t *testing.T) {
+	oldSlice := NewSlice(0, 10, 1)
+	newSlice := Shuffle(oldSlice)
+	for _, newNumber := range newSlice {
+		for i, oldNumber := range oldSlice {
+			if oldNumber == newNumber {
+				// Delete find element.
+				oldSlice = append(oldSlice[:i], oldSlice[i+1:]...)
+			}
+		}
+	}
+	if len(oldSlice) != 0 {
+		t.Errorf("Test generate random masternode fail %v - %v", oldSlice, newSlice)
+	}
+}
+
+func TestEncryptDecrypt(t *testing.T) {
+	//byteInteger := common.LeftPadBytes([]byte(new(big.Int).SetInt64(4).String()), 32)
+	randomByte := RandStringByte(32)
+	encrypt := Encrypt(randomByte, new(big.Int).SetInt64(4).String())
+	decrypt := Decrypt(randomByte, encrypt)
+	t.Log("Encrypt", encrypt, "Test", string(randomByte), "Decrypt", decrypt, "trim", string(bytes.TrimLeft([]byte(decrypt), "\x00")))
+}
+
+func TestSendTxRandomizeSecretAndOpening(t *testing.T) {
+	backend := getCommonBackend()
+	signer := types.HomesteadSigner{}
+	ctx := context.Background()
+
+	transactOpts := bind.NewKeyedTransactor(acc1Key)
+	transactOpts.GasLimit = 4200000
+	epocNumber := uint64(99)
+	randomizeAddr, randomizeContract, err := randomize.DeployRandomize(transactOpts, backend, new(big.Int).SetInt64(99))
+	if err != nil {
+		t.Fatalf("Can't deploy randomize SC: %v", err)
+	}
+	backend.Commit()
+
+	nonce := uint64(1)
+	randomizeKeyValue := RandStringByte(32)
+	tx, err := BuildTxSecretRandomize(nonce, randomizeAddr, epocNumber, randomizeKeyValue)
+	if err != nil {
+		t.Fatalf("Can't create tx randomize secret: %v", err)
+	}
+	tx, err = types.SignTx(tx, signer, acc1Key)
+	if err != nil {
+		t.Fatalf("Can't sign tx randomize secret: %v", err)
+	}
+
+	err = backend.SendTransaction(ctx, tx)
+	if err != nil {
+		t.Fatalf("Can't send tx for create randomize secret: %v", err)
+	}
+	backend.Commit()
+	// Increment nonce.
+	nonce++
+	// Set opening.
+	tx, err = BuildTxOpeningRandomize(nonce, randomizeAddr, randomizeKeyValue)
+	if err != nil {
+		t.Fatalf("Can't create tx randomize opening: %v", err)
+	}
+	tx, err = types.SignTx(tx, signer, acc1Key)
+	if err != nil {
+		t.Fatalf("Can't sign tx randomize opening: %v", err)
+	}
+
+	err = backend.SendTransaction(ctx, tx)
+	if err != nil {
+		t.Fatalf("Can't send tx for create randomize opening: %v", err)
+	}
+	backend.Commit()
+
+	// Get randomize secret from SC.
+	secretsArr, err := randomizeContract.GetSecret(acc1Addr)
+	if err != nil {
+		t.Fatalf("Can't get secret from SC: %v", err)
+	}
+	if len(secretsArr) <= 0 {
+		t.Error("Empty get secrets from SC", err)
+	}
+
+	// Decrypt randomize from SC.
+	secrets, err := randomizeContract.GetSecret(acc1Addr)
+	if err != nil {
+		t.Error("Fail get secrets from randomize", err)
+	}
+	opening, err := randomizeContract.GetOpening(acc1Addr)
+	if err != nil {
+		t.Fatalf("Can't get secret from SC: %v", err)
+	}
+	randomizes, err := decryptRandomizeFromSecretsAndOpening(secrets, opening)
+	if err != nil {
+		t.Error("Can't decrypt secret and opening", err)
+	}
+	if len(randomizes) != 991 {
+		t.Error("Randomize length not match")
+	}
+}
+
+func isArrayEqual(a [][]int64, b [][]int64) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, vs := range a {
+		for j, v := range vs {
+			if v != b[i][j] {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func TestTransposeMatrix(t *testing.T) {
+	a := [][]int64{
+		{0, 1, 2, 3, 4},
+		{4, 5, 6, 7, 8},
+	}
+	b := [][]int64{
+		{0, 4},
+		{1, 5},
+		{2, 6},
+		{3, 7},
+		{4, 8},
+	}
+	if !isArrayEqual(b, TransposeMatrix(a)) {
+		t.Errorf("Fail to transpose matrix %v - %v", a, TransposeMatrix(a))
+	}
+}
+
+func TestMultiMatrix(t *testing.T) {
+	a := [][]int64{
+		{1, -1, -1, 1, 1, -1},
+	}
+	b := make([][]int64, 6)
+	for i := 0; i < len(a[0]); i++ {
+		b[i] = Shuffle(NewSlice(0, 6, 1))
+	}
+	c, err := DotMatrix(a, b)
+	if err != nil {
+		t.Error("Fail to test dot matrix", err)
+	}
+	if len(a[0]) != len(c[0]) {
+		t.Errorf("Fail to test dot matrix result %v - %v - %v", a, b, c)
+	}
+}
+
+// Unit test for
+func TestGenM2FromRandomize(t *testing.T) {
+	a := [][]int64{
+		{8, 4, 1, 5, 0, 3, 7, 6, 9, 2, 1},
+		{4, 0, 8, 3, 1, 7, 9, 2, 5, 6, -1},
+		{8, 1, 3, 2, 0, 9, 7, 4, 6, 5, -1},
+		{2, 5, 4, 0, 6, 3, 1, 8, 7, 9, 1},
+		{3, 7, 4, 1, 8, 2, 0, 9, 6, 5, 1},
+		{1, 4, 8, 3, 7, 5, 2, 6, 0, 9, -1},
+		{7, 4, 2, 0, 8, 1, 9, 6, 5, 3, 1},
+		{4, 2, 5, 3, 1, 0, 7, 9, 6, 8, 1},
+		{0, 2, 4, 7, 1, 8, 6, 3, 5, 9, -1},
+		{7, 4, 3, 9, 1, 5, 0, 6, 8, 2, -1},
+	}
+	b, err := GenM2FromRandomize(a)
+	if err != nil {
+		t.Error("Fail to test gen m2 for randomize.", err)
+	}
+	c := []int64{6, 4, 0, 8, 5, 9, 1, 2, 0, 4}
+	if !isArrayEqual([][]int64{b}, [][]int64{c}) {
+		t.Errorf("Fail to get m2 result %v - %v", a, b)
+	}
 }

TAO-STAR425/TAO

contracts/utils_test.go
@@ -16,6 +16,7 @@
 package contracts
 
 import (
+	"bytes"
 	"context"
 	"crypto/ecdsa"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
@@ -31,22 +32,30 @@ import (
 	"time"
 )
 
-func TestSendTxSign(t *testing.T) {
-	acc1Key, _ := crypto.HexToECDSA("8a1f9a8f95be41cd7ccb6168179afb4504aefe388d1e14474d32c45c72ce7b7a")
-	acc2Key, _ := crypto.HexToECDSA("49a7b37aa6f6645917e7b807e9d1c00d4fa71f18343b0d4122a4d2df64dd6fee")
-	acc3Key, _ := crypto.HexToECDSA("b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291")
-	acc4Key, _ := crypto.HexToECDSA("b71c71a67e1177ad4e901695e1b4b9ee04aefe388d1e14474d32c45c72ce7b7a")
-	acc1Addr := crypto.PubkeyToAddress(acc1Key.PublicKey)
-	acc2Addr := crypto.PubkeyToAddress(acc2Key.PublicKey)
-	acc3Addr := crypto.PubkeyToAddress(acc3Key.PublicKey)
-	acc4Addr := crypto.PubkeyToAddress(acc4Key.PublicKey)
-	accounts := []common.Address{acc2Addr, acc3Addr, acc4Addr}
-	keys := []*ecdsa.PrivateKey{acc2Key, acc3Key, acc4Key}
+var (
+	acc1Key, _ = crypto.HexToECDSA("8a1f9a8f95be41cd7ccb6168179afb4504aefe388d1e14474d32c45c72ce7b7a")
+	acc2Key, _ = crypto.HexToECDSA("49a7b37aa6f6645917e7b807e9d1c00d4fa71f18343b0d4122a4d2df64dd6fee")
+	acc3Key, _ = crypto.HexToECDSA("b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291")
+	acc4Key, _ = crypto.HexToECDSA("b71c71a67e1177ad4e901695e1b4b9ee04aefe388d1e14474d32c45c72ce7b7a")
+	acc1Addr   = crypto.PubkeyToAddress(acc1Key.PublicKey)
+	acc2Addr   = crypto.PubkeyToAddress(acc2Key.PublicKey)
+	acc3Addr   = crypto.PubkeyToAddress(acc3Key.PublicKey)
+	acc4Addr   = crypto.PubkeyToAddress(acc4Key.PublicKey)
+)
 
-	signer := types.HomesteadSigner{}
-	genesis := core.GenesisAlloc{acc1Addr: {Balance: big.NewInt(1000000000)}}
+func getCommonBackend() *backends.SimulatedBackend {
+	genesis := core.GenesisAlloc{acc1Addr: {Balance: big.NewInt(1000000000000)}}
 	backend := backends.NewSimulatedBackend(genesis)
 	backend.Commit()
+
+	return backend
+}
+
+func TestSendTxSign(t *testing.T) {
+	accounts := []common.Address{acc2Addr, acc3Addr, acc4Addr}
+	keys := []*ecdsa.PrivateKey{acc2Key, acc3Key, acc4Key}
+	backend := getCommonBackend()
+	signer := types.HomesteadSigner{}
 	ctx := context.Background()
 
 	transactOpts := bind.NewKeyedTransactor(acc1Key)
@@ -114,4 +123,119 @@ func randomHash() common.Hash {
 		b[i] = letterBytes[rand.Intn(len(letterBytes))]
 	}
 	return b
+}
+
+// Unit test for get random position of masternodes.
+func TestRandomMasterNode(t *testing.T) {
+	oldSlice := NewSlice(0, 10, 1)
+	newSlice := Shuffle(oldSlice)
+	for _, newNumber := range newSlice {
+		for i, oldNumber := range oldSlice {
+			if oldNumber == newNumber {
+				// Delete find element.
+				oldSlice = append(oldSlice[:i], oldSlice[i+1:]...)
+			}
+		}
+	}
+	if len(oldSlice) != 0 {
+		t.Errorf("Test generate random masternode fail %v - %v", oldSlice, newSlice)
+	}
+}
+
+func TestEncryptDecrypt(t *testing.T) {
+	//byteInteger := common.LeftPadBytes([]byte(new(big.Int).SetInt64(4).String()), 32)
+	randomByte := RandStringByte(32)
+	encrypt := Encrypt(randomByte, new(big.Int).SetInt64(4).String())
+	decrypt := Decrypt(randomByte, encrypt)
+	t.Log("Encrypt", encrypt, "Test", string(randomByte), "Decrypt", decrypt, "trim", string(bytes.TrimLeft([]byte(decrypt), "\x00")))
+}
+
+func TestTransposeMatrix(t *testing.T) {
+	a := [][]int64{
+		{0, 1, 2, 3, 4},
+		{4, 5, 6, 7, 8},
+	}
+	b := [][]int64{
+		{0, 4},
+		{1, 5},
+		{2, 6},
+		{3, 7},
+		{4, 8},
+	}
+	if !isArrayEqual(b, TransposeMatrix(a)) {
+		t.Errorf("Fail to transpose matrix %v - %v", a, TransposeMatrix(a))
+	}
+}
+
+func TestMultiMatrix(t *testing.T) {
+	a := make([][]int64, 6)
+	b := [][]int64{
+		{1, -1, -1, 1, 1, -1},
+	}
+	for i := 0; i < len(b[0]); i++ {
+		a[i] = Shuffle(NewSlice(0, 6, 1))
+	}
+	c, err := DotMatrix(a, b)
+	if err != nil {
+		t.Error("Fail to test dot matrix", err)
+	}
+	if len(a[0]) != len(c) {
+		t.Errorf("Fail to test dot matrix result %v - %v - %v", a, b, c)
+	}
+}
+
+func isArrayEqual(a [][]int64, b [][]int64) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, vs := range a {
+		for j, v := range vs {
+			if v != b[i][j] {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+// Unit test for
+func TestGenM2FromRandomize(t *testing.T) {
+	a := [][]int64{
+		{37, 23, 17, 45, 38, 8, 21, 28, 15, 41, 1, 25, 4, 30, 31, 0, 9, 16, 46, 13, 36, 7, 19, 27, 47, 32, 22, 3, 20, 33, 2, 35, 49, 6, 42, 34, 44, 10, 29, 26, 12, 43, 48, 24, 40, 14, 18, 39, 5, 11, 1},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{28, 16, 13, 31, 32, 36, 44, 14, 37, 33, 3, 23, 17, 46, 35, 30, 45, 27, 9, 41, 7, 19, 10, 24, 5, 34, 29, 18, 21, 15, 0, 2, 25, 39, 11, 4, 22, 6, 48, 42, 12, 26, 1, 47, 43, 20, 40, 38, 8, 49, -1},
+	}
+	b, err := GenM2FromRandomize(a)
+	t.Log("randomize", b)
+	if err != nil {
+		t.Error("Fail to test gen m2 for randomize.", err)
+	}
+	c := []int64{0, 1, 1, 2, 0, 1, 2, 2, 1, 2, 2, 2, 1, 1, 1, 0, 0, 2, 1, 1, 2, 0, 0, 0, 0, 2, 1, 0, 1, 0, 2, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 2, 2, 2, 0, 0, 1, 1, 0, 0, 0}
+	if !isArrayEqual([][]int64{b}, [][]int64{c}) {
+		t.Errorf("Fail to get m2 result %v", b)
+	}
+}
+
+// Unit test for validator m2.
+func TestBuildValidatorFromM2(t *testing.T) {
+	a := []int64{0, 1, 1, 2, 0, 1, 2, 128, 150, 2, 2, 2, 1, 1, 1, 0, 0, 2, 1, 1, 2, 0, 0, 0, 0, 2, 1, 0, 1, 0, 2, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 2, 2, 2, 0, 0, 1, 1, 0, 0, 0}
+	b := BuildValidatorFromM2(a)
+	c := ExtractValidatorsFromBytes(b)
+	if !isArrayEqual([][]int64{a}, [][]int64{c}) {
+		t.Errorf("Fail to get m2 result %v", b)
+	}
+}
+
+// Unit test for decode validator string data.
+func TestDecodeValidatorsHexData(t *testing.T) {
+	a := "0x000000310000003000000032000000310000003000000032000000310000003000000032000000310000003000000031000000320000003000000031000000320000003000000031000000320000003000000030000000310000003200000030000000310000003200000030000000310000003200000030000000300000003100000032000000300000003100000032000000300000003100000032000000300000003200000030000000310000003200000030000000310000003200000030000000310000003000000030"
+	b, err := DecodeValidatorsHexData(a)
+	if err != nil {
+		t.Error("Fail to decode validator from hex string", err)
+	}
+	c:= []int64{1, 0, 2, 1, 0, 2, 1, 0, 2, 1, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 2, 0, 1, 2, 0, 1, 2, 0, 1, 0, 0}
+	if !isArrayEqual([][]int64{b}, [][]int64{c}) {
+		t.Errorf("Fail to get m2 result %v", b)
+	}
+	t.Log("b", b)
 }